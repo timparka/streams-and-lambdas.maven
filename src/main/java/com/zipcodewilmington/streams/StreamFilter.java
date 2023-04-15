2ef4a36bfb5cb31922dad07e6c14b6459db84d7a
Tim Park <pimtarka@gmail.com>
2023-04-14 20:20:08 -0400

    finished lab

--- src/main/java/com/zipcodewilmington/streams/StreamFilter.java
+++ src/main/java/com/zipcodewilmington/streams/StreamFilter.java
@@ -20,7 +20,7 @@ public class StreamFilter {
      * No arg constructor
      */ //TODO - construct person stream of 100 person objects; startingCharacter is a random capital letter
     public StreamFilter() {
-        this(Stream.empty(), null);
+        this(new PersonFactory().createPersonStream(100), (char) (Math.random() * (90 - 65) + 65));
     }
 
     /**
@@ -28,7 +28,7 @@ public class StreamFilter {
      * @param startingCharacter - character to filter by
      */ //TODO
     public StreamFilter(Person[] people, Character startingCharacter) {
-        this(Stream.empty(), null);
+        this(Stream.of(people),startingCharacter);
     }
 
     /**
@@ -36,7 +36,7 @@ public class StreamFilter {
      * @param startingCharacter - character to filter by
      */ //TODO
     public StreamFilter(List<Person> people, Character startingCharacter) {
-        this(Stream.empty(), null);
+        this(people.stream(), startingCharacter);
     }
 
 
@@ -55,7 +55,10 @@ public class StreamFilter {
      * @return a list of person object whose name starts with `this.startingCharacter`
      */ //TODO
     public List<Person> toListMultiLine() {
-        return null;
+        return personStream
+                .filter((person -> person.getName()
+                .startsWith(startingCharacter)))
+                .collect(Collectors.toList());
     }
 
 
@@ -64,7 +67,7 @@ public class StreamFilter {
      * @return a list of person objects whose name starts with `this.startingCharacter`
      */ //TODO
     public List<Person> toListOneLine() {
-        return null;
+        return personStream.filter(person -> person.getName().startsWith(startingCharacter)).collect(Collectors.toList());
     }
 
 
@@ -73,7 +76,7 @@ public class StreamFilter {
      * @return an array of person object whose name starts with `this.startingCharacter`
      */ //TODO
     public Person[] toArrayOneLine() {
-        return null;
+        return toListOneLine().toArray(new Person[0]);
     }
 
 
@@ -82,7 +85,7 @@ public class StreamFilter {
      * @return an array of person object whose name starts with `this.startingCharacter`
      */ //TODO
     public Person[] toArrayMultiLine() {
-        return null;
+        return toListMultiLine().toArray(new Person[0]);
     }
     public static void main(String[] args) {
         
--- src/main/java/com/zipcodewilmington/streams/anthropoid/PersonFactory.java
+++ src/main/java/com/zipcodewilmington/streams/anthropoid/PersonFactory.java
@@ -3,6 +3,7 @@ package com.zipcodewilmington.streams.anthropoid;
 import com.zipcodewilmington.streams.tools.RandomUtils;
 import com.zipcodewilmington.streams.tools.StringUtils;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -14,7 +15,9 @@ import java.util.stream.Stream;
  * @ATTENTION_TO_STUDENTS You are FORBIDDEN from using loops of any sort within the definition of this class.
  */
 public final class PersonFactory {
+    List<Person> persons;
     public PersonFactory() {
+        this.persons = new ArrayList<>();
         /** this class is not to be instantiated */
     }
 
